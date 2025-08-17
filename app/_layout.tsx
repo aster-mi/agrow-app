3401393eabcfc2f244400a22fb094d3fdedefb54
aster-mi <79004570+aster-mi@users.noreply.github.com>
2025-08-17 17:29:52 +0900

    feat: enable posting to timeline

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,14 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '../hooks/useFrameworkReady';
+import { initDb } from '../db';
 
 export default function RootLayout() {
   useFrameworkReady();
+
+  useEffect(() => {
+    initDb();
+  }, []);
 
   return (
     <>
